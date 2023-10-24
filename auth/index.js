2d3e7b91b566f78d082d9361866752775bdca48f
pascal-h-kim <hyun.kim@knowre.com>
2023-10-24 19:16:49 +0900

    auth

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-require('express-async-errors');
 const sequelize = require('./database');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
